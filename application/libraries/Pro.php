137d097db9b91a8f81b75bf50779de5816098e14
sojwal123 <patilsojwal123@gmail.com>
2018-07-06 10:55:32 +0530

    add library

---
+++ application/libraries/Pro.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Pro
+ {
+
+    public function myfunction()
+		{
+		  return "Vaibhav chutya aahe.";
+		}
+ }
+?>
