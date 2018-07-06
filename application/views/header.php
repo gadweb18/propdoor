16cb83216b661fd7d01091546ddab29139306ee4
anil92 <kurmianilkumar3@gmail.com>
2018-07-06 15:10:34 +0530

    structure

---
+++ application/views/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!-- 
+Template Name: Real Estate
+Version: 1.0.0
+-->
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+
+<!-- Mirrored from themeforest.swanitinfotech.com/real_estate/demo/real_estate/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 05 Jul 2018 08:39:45 GMT -->
+<head>
+	<meta charset="utf-8" />
+	<title>Real Estate Responsive HTML Template</title>
+	<meta content="width=device-width, initial-scale=1.0" name="viewport" />
+	<meta name="description"  content="Real Estate"/>
+	<meta name="keywords" content="Real Estate, Property template, Real Estate template, rent, sale, buy" />
+	<meta name="author"  content=""/>
+	<meta name="MobileOptimized" content="320" />
+	<!--srart theme style -->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.css');?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/font-awesome.css');?>"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/fonts.css');?>"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/owl.carousel.css');?>"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/owl.theme.default.css');?>"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/jquery-ui.css');?>"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css');?>"/>
+	<!-- favicon links -->
+	<link rel="shortcut icon" type="image/png" href="<?php echo base_url('assets/images/header/favicon.png');?>" />
+</head>
---
+++ application/views/menu.php
@@ -0,0 +1,61 @@
+<div class="rle_header_wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+					<div class="rle_logo">
+						<a href="#"><img src="<?php echo base_url('assets/images/icon/logo.png');?>" alt="Logo" title="Logo"></a>
+					</div>
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+					<button class="rle_menu_btn"><i class="fa fa-bars" aria-hidden="true"></i></button>
+					<div class="rle_main_menu_wrapper">
+						<div class="rle_main_menu">
+							<ul>
+								<li><a href="#">propdoor approved</a>
+								</li>
+								<li><a href="#">ABOUT US</a>
+								</li>
+								<li><a href="#">multiportal search</a></li>
+								<li><a href="#">investment</a>
+									<ul class="sub-menu">
+										<li><a href="#">money investment</a></li>
+										<li><a href="#">land investment</a></li>
+										<li><a href="#">money investment</a></li>
+									</ul>
+								</li>
+								<li><a href="#">other</a>
+									<ul class="sub-menu">
+										<li><a href="#">legal</a></li>
+										<li><a href="#">loan</a></li>
+										<li><a href="#">title search</a></li>
+										<li><a href="#">rera</a></li>
+										<li><a href="#">more</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="rle_signin">
+						<a href="#" class="rle_btn" id="login_button">sign in</a>
+						<div id="login_one" class="rle_login_form">
+							<h4 style="text-align: center;">Login</h4>
+							<form class="form">
+								<input type="text" placeholder="Username">
+								<input type="password" placeholder="Password">
+								<div class="rle_checkbox">
+									<input type="checkbox" id="remember_me" checked />
+									<label for="remember_me">Remember me</label>
+								</div>
+								<a href="forgot-pass.html" class="forgot_pswd">forgot password?</a>
+								<button type="submit" class="rle_btn">sign in</button>
+								<span>or</span>
+								<a href="#" class="share_btn fcbk_clr_1"><i class="fa fa-facebook" aria-hidden="true"></i>login with facebook</a>
+								<a href="#" class="share_btn ggl_clr_2"><i class="fa fa-google-plus" aria-hidden="true"></i>login with google plus</a>
+							</form>
+							<p style="text-align: center;">Are you a Real Estate Professional?<a href="signup.html" target="_blank"> Register here.</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
