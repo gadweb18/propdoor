95bd123afba2c03803d25dac52b11da968a6f714
sojwal123 <patilsojwal123@gmail.com>
2018-07-06 12:42:43 +0530

    add config fb and glg

---
+++ application/config/googleplus.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['googleplus']['application_name'] = '';
+$config['googleplus']['client_id']        = '297154047503-fhob9ttp4r5unpshm0njq1kcpv5s5uig.apps.googleusercontent.com';
+$config['googleplus']['client_secret']    = 'jYN1QHYVCQqFu-hDWTkNMLLB';
+$config['googleplus']['redirect_uri']     = 'http://localhost/fblog/index.php/Google_login/googlelogin';
+$config['googleplus']['api_key']          = '';
+$config['googleplus']['scopes']           = array();
+
+?>
